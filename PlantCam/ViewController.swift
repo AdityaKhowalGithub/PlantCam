2f006b37c079eca42c748d54aa06f7d1f0f8ef18
Aditya Khowal <adityakh2003@outlook.com>
2023-09-05 12:33:51 -0700

    Initial Commit

---
+++ PlantCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlantCam
+//
+//  Created by Aditya Khowal on 9/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
