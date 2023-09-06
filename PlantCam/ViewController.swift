a43222d99ae007e1fb7ad8640011335c4bc8f6cd
Aditya Khowal <adityakh2003@outlook.com>
2023-09-05 20:47:26 -0700

    Added text to screen

--- PlantCam/ViewController.swift
+++ PlantCam/ViewController.swift
@@ -8,14 +8,27 @@
 import UIKit
 import AVKit
 import Vision
-class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    let identifierLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .white
+        
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupIdentifierConfidenceLabel()
+        
         // Do any additional setup after loading the view.
         let captureSession = AVCaptureSession()
         guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
-                
+        captureSession.sessionPreset = .photo
         guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
         
         captureSession.addInput(input)
@@ -26,47 +39,47 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         view.layer.addSublayer(previewLayer)
         previewLayer.frame = view.frame
         
-        
         let dataOutput = AVCaptureVideoDataOutput()
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "Video Queue"))
         captureSession.addOutput(dataOutput)
-
-        
-
-        
-        //        let request = VNCoreMLRequest(model: <#T##VNCoreMLModel#>)
-//
-//        VNImageRequestHandler(cgImage: <#T##CGImage#>, options: [:]).perform(<#T##requests: [VNRequest]##[VNRequest]#>)
-
+        setupIdentifierConfidenceLabel()
     }
     
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        //print("capture successful", Date())
-        
-        guard let pixelbuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
-        
-        guard let model = try? VNCoreMLModel(for: MobileNetV2().model) else {return}
-        
-        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
-            
-//            print(finishedReq.results)
-            
-            guard let results = finishedReq.results as?
-                    [VNClassificationObservation] else {return}
-            guard let firstObservation = results.first else {return}
-            
-            print(firstObservation.identifier, firstObservation.confidence)
+    fileprivate func setupIdentifierConfidenceLabel() {
+        view.addSubview(identifierLabel)
+        identifierLabel.textColor = .black
 
-            
+        identifierLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32).isActive = true
+        identifierLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        identifierLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        identifierLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelbuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        let config = MLModelConfiguration()
+        guard let coreMLModel = try? MobileNetV2(configuration: config),
+              let visionModel = try? VNCoreMLModel(for: coreMLModel.model) else {
+            return
         }
         
-
-
-        //
+        let request = VNCoreMLRequest(model: visionModel) { (finishedReq, err) in
+            if let error = err {
+                print("Error: \(error.localizedDescription)")
+            } else if let results = finishedReq.results as? [VNClassificationObservation], let firstObservation = results.first {
+                //print(firstObservation.identifier, firstObservation.confidence)
+                DispatchQueue.main.async {
+                    self.identifierLabel.text = "\(firstObservation.identifier) \(firstObservation.confidence*100)"
+                }
+            } else {
+                print("No results or error")
+            }
+        }
         
         try? VNImageRequestHandler(cvPixelBuffer: pixelbuffer, options: [:]).perform([request])
     }
 
 
 }
-
