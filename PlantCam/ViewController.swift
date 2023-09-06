4ae4edcb4c2703c4b94938e34d5f6be9674dc050
Aditya Khowal <adityakh2003@outlook.com>
2023-09-06 00:47:01 -0700

    added slider for buffer rate and text on screen works

--- PlantCam/ViewController.swift
+++ PlantCam/ViewController.swift
@@ -10,21 +10,38 @@ import AVKit
 import Vision
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
-    
-    let identifierLabel: UILabel = {
-        let label = UILabel()
-        label.backgroundColor = .white
+    var N: Int = 20
+        let identifierLabel: UILabel = {
+            let label = UILabel()
+            label.backgroundColor = .white
+            
+            label.textAlignment = .center
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
         
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setupIdentifierConfidenceLabel()
-        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            let slider = UISlider()
+            slider.minimumValue = 1
+            slider.maximumValue = 100
+            slider.value = Float(N)
+            view.addSubview(slider)
+            // Add constraints to position the slider
+            slider.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                slider.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                slider.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+                slider.widthAnchor.constraint(equalToConstant: 200)
+            ])
+
+            
+            // Add an action to the slider
+            slider.addTarget(self, action: #selector(sliderChanged), for: .valueChanged)
+            
+            setupIdentifierConfidenceLabel()
+            
         // Do any additional setup after loading the view.
         let captureSession = AVCaptureSession()
         guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
@@ -43,6 +60,13 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "Video Queue"))
         captureSession.addOutput(dataOutput)
         setupIdentifierConfidenceLabel()
+        
+        
+        
+    }
+    // Update the value of N when the slider's value changes
+    @objc func sliderChanged(_ sender: UISlider) {
+        N = Int(sender.value)
     }
     
     fileprivate func setupIdentifierConfidenceLabel() {
@@ -54,7 +78,15 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         identifierLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         identifierLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
+
+
     
+    // Buffer to hold last N predictions
+    var predictionBuffer: [String] = []
+    // Add a slider to your view
+   //let N = 20
+
+   
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let pixelbuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
@@ -69,9 +101,16 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             if let error = err {
                 print("Error: \(error.localizedDescription)")
             } else if let results = finishedReq.results as? [VNClassificationObservation], let firstObservation = results.first {
-                //print(firstObservation.identifier, firstObservation.confidence)
                 DispatchQueue.main.async {
-                    self.identifierLabel.text = "\(firstObservation.identifier) \(firstObservation.confidence*100)"
+                    // Add new prediction to buffer and remove oldest if buffer is full
+                    self.predictionBuffer.append(firstObservation.identifier)
+                    if self.predictionBuffer.count > self.N {
+                        self.predictionBuffer.removeFirst()
+                    }
+
+                    // Update label with most frequent prediction in buffer
+                    let mostFrequentPrediction = self.predictionBuffer.mostFrequent()
+                    self.identifierLabel.text = "\(mostFrequentPrediction) \(firstObservation.confidence*100)"
                 }
             } else {
                 print("No results or error")
@@ -82,4 +121,13 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     }
 
 
+
+}
+extension Array where Element: Hashable {
+    func mostFrequent() -> Element? {
+        let counts = self.reduce(into: [:]) { (counts: inout [Element: Int], element) in
+            counts[element, default: 0] += 1
+        }
+        return counts.max { $0.1 < $1.1 }?.key
+    }
 }
